baa93e07cc62a329f10304395b43664f856b0c7a
Orangel Valdespino <orangelvc93@gmail.com>
2024-07-03 15:00:10 -0500

    Primer commit

---
+++ components/Logo.tsx
@@ -0,0 +1,7 @@
+import { FaPrint } from "react-icons/fa"
+
+export const Logo = () => {
+    return (
+        <div className="flex relative z-20 items-center text-lg font-medium space-x-3"><FaPrint /> <p>Administrador de Consumibles</p></div>
+    )
+}
